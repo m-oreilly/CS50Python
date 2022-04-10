9dcaa76958ee59125418e8287476ba539c7128ef
Michael O'Reilly <NorticaDND@gmail.com>
2022-04-10 22:17:34 +0100

    einstein 2nd draft

--- Problem Set 1/einstein.py
+++ Problem Set 1/einstein.py
@@ -1,7 +1,10 @@
 def main():
+    # Set varaible for the speed of light
     c = 300000000
+
     m = get_user_input()
     e = calc_joules(m, c)
+
     # Print the Joules back to the user
     print("E:", e)
 
@@ -11,7 +14,7 @@ def get_user_input():
     return int(input("m: "))
 
 
-# Calculate the Joules
+# Returns Energy, takes in arguements Mass and Speed of light
 def calc_joules(mass, speedoflight):
     return mass * (speedoflight ** 2)
 

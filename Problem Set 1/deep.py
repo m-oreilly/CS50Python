66fa86da57615a10a23b4881ff50e9a7f9dea14c
Michael O'Reilly <NorticaDND@gmail.com>
2022-04-12 23:17:56 +0100

    Refactor to simplify codebase

--- Problem Set 1/deep.py
+++ Problem Set 1/deep.py
@@ -1,35 +1,25 @@
 def main():
-    # Initialise variable
-    x = None
 
     # Start loop, to repeat until correct answer is given
-    while check_for_equality(x) is False:
-        x = great_question_answer()
-        x = clean_up_answer(x)
-        if check_for_equality(x) is True:
+    while True:
+        users_answer = great_question_answer()
+
+        if check_for_equality(users_answer):
             print("Yes")
+            break
         else:
             print("No")
 
 
 # Ask the great question
 def great_question_answer():
-    return input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
-
-
-# Clean up user input
-def clean_up_answer(dirty_answer):
-    return dirty_answer.strip().replace("-", " ").lower()
+    return input("What is the Answer to the Great Question of Life, "
+                 "the Universe, and Everything? ").strip().replace("-", " ").lower()
 
 
 # Check for equality
 def check_for_equality(clean_answer):
-    if clean_answer == "42":
-        return True
-    elif clean_answer == "forty two":
-        return True
-    else:
-        return False
+    return clean_answer == "42" or clean_answer == "forty two"
 
 
 # Call main to run

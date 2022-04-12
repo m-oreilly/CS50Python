51f7f21b80b4ec91001745289b2414a19d2fc62b
Michael O'Reilly <NorticaDND@gmail.com>
2022-04-13 00:11:48 +0100

    Refactor

--- Problem Set 0/einstein.py
+++ Problem Set 0/einstein.py
@@ -3,7 +3,7 @@ def main():
     c = 300000000
 
     # Ask for Mass, Calculate energy
-    m = get_user_input()
+    m = get_mass()
     e = calc_joules(m, c)
 
     # Print the Energy back to the user
@@ -11,13 +11,13 @@ def main():
 
 
 # Get the users input
-def get_user_input():
+def get_mass():
     return int(input("m: "))
 
 
-# Returns Energy, takes in arguements Mass and Speed of light
-def calc_joules(mass, speedoflight):
-    return mass * (speedoflight ** 2)
+# Returns Energy, takes in Mass and Speed of light
+def calc_joules(mass, speed_of_light):
+    return mass * (speed_of_light ** 2)
 
 
 main()

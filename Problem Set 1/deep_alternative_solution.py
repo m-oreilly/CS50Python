74e2bd9c7b1f86f84ef9b4cab149cc11c4777096
Michael O'Reilly <NorticaDND@gmail.com>
2022-04-12 23:54:35 +0100

    More refactor

--- Problem Set 1/deep_alternative_solution.py
+++ Problem Set 1/deep_alternative_solution.py
@@ -1,11 +1,12 @@
 def main():
+    the_great_question = "What is the Answer to the Great Question of Life, the Universe, and Everything? "
     the_answers = ["42", "forty two"]
 
     # Start loop, to repeat until correct answer is given
     while True:
-        users_guess = answer_to_great_question()
+        users_guess = get_answer_to_great_question(the_great_question)
 
-        if check_for_equality(users_guess, the_answers):
+        if check_guess(users_guess, the_answers):
             print("Yes")
             break
         else:
@@ -13,13 +14,12 @@ def main():
 
 
 # Ask the great question
-def answer_to_great_question():
-    return input("What is the Answer to the Great Question of Life, "
-                 "the Universe, and Everything? ").strip().replace("-", " ").lower()
+def get_answer_to_great_question(question):
+    return input(question).strip().replace("-", " ").lower()
 
 
 # Check for equality
-def check_for_equality(guess, answer):
+def check_guess(guess, answer):
     return guess in answer
 
 
